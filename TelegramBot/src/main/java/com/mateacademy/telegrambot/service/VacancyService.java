61fde71a373c325fe4c70cf6f0b238a3409e7078
Dmytro Ivanenko <d.ivanenko@andersenlab.com>
2023-08-30 00:13:41 +0200

    Added CSV files reading and use logic

--- TelegramBot/src/main/java/com/mateacademy/telegrambot/service/VacancyService.java
+++ TelegramBot/src/main/java/com/mateacademy/telegrambot/service/VacancyService.java
@@ -3,6 +3,7 @@ package com.mateacademy.telegrambot.service;
 import com.mateacademy.telegrambot.dto.VacancyDto;
 import com.mateacademy.telegrambot.utils.ProficiencyLevel;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -14,25 +15,15 @@ public class VacancyService {
 
     private final Map<String, VacancyDto> vacancies = new HashMap<>();
 
+    @Autowired
+    private VacanciesReaderService vacanciesReaderService;
+
     @PostConstruct
     public void init() {
-        VacancyDto junVacancy = new VacancyDto(
-                "1",
-                "Java Junior Developer",
-                "Java Core, Java Collection Framework");
-        VacancyDto midVacancy = new VacancyDto(
-                "2",
-                "Java Middle Developer",
-                "Spring Boot, Spring Security, Spring Data");
-        VacancyDto sinVacancy = new VacancyDto(
-                "3",
-                "Java Senior Developer",
-                "Microservices Patterns, AWS, GCP");
-
-        vacancies.put("1", junVacancy);
-        vacancies.put("2", midVacancy);
-        vacancies.put("3", sinVacancy);
-
+        List<VacancyDto> list = vacanciesReaderService.getVacanciesFromFile("vacancies.csv");
+        for (VacancyDto vacancy : list) {
+            vacancies.put(vacancy.getId(), vacancy);
+        }
     }
 
     public List<VacancyDto> findVacancies(ProficiencyLevel level) {
