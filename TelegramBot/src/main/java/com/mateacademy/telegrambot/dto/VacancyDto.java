c48b2ad6fed5847c2c0f6673daf90bc05bd90dfe
Dmytro Ivanenko <d.ivanenko@andersenlab.com>
2023-08-30 00:12:55 +0200

    Update DTO to work with CSV files

--- TelegramBot/src/main/java/com/mateacademy/telegrambot/dto/VacancyDto.java
+++ TelegramBot/src/main/java/com/mateacademy/telegrambot/dto/VacancyDto.java
@@ -1,5 +1,6 @@
 package com.mateacademy.telegrambot.dto;
 
+import com.opencsv.bean.CsvBindByName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,24 @@ import lombok.NoArgsConstructor;
 @Data
 public class VacancyDto {
 
+    @CsvBindByName(column = "Id")
     private String id;
+
+    @CsvBindByName(column = "Title")
     private String title;
+
+    @CsvBindByName(column = "Short description")
     private String shortDescription;
+
+    @CsvBindByName(column = "Long description")
+    private String longDescription;
+
+    @CsvBindByName(column = "Company")
+    private String company;
+
+    @CsvBindByName(column = "Salary")
+    private String salary;
+
+    @CsvBindByName(column = "Link")
+    private String link;
 }
