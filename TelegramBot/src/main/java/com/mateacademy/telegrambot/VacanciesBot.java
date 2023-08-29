60ee6271b648cc5c66a636f82f78359b67dc4078
Dmytro Ivanenko <107722608+Ivanenk0@users.noreply.github.com>
2023-08-29 14:49:57 +0200

    Added welcome message & start menu with action buttons (#2)

--- TelegramBot/src/main/java/com/mateacademy/telegrambot/VacanciesBot.java
+++ TelegramBot/src/main/java/com/mateacademy/telegrambot/VacanciesBot.java
@@ -2,7 +2,15 @@ package com.mateacademy.telegrambot;
 
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class VacanciesBot extends TelegramLongPollingBot {
@@ -15,7 +23,40 @@ public class VacanciesBot extends TelegramLongPollingBot {
     // User actions handling method
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println("Event received");
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(update.getMessage().getChatId());
+        sendMessage.setText("Welcome to JAVA vacancies bot\nSelect your preferences in proficiency level");
+        sendMessage.setReplyMarkup(getStartMenu());
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ReplyKeyboard getStartMenu() {
+        List<InlineKeyboardButton> proficiencyLevelButtons = new ArrayList<>();
+
+        InlineKeyboardButton juniorButton = new InlineKeyboardButton();
+        juniorButton.setText("JUNIOR");
+        juniorButton.setCallbackData("showJuniorVacancies");
+        proficiencyLevelButtons.add(juniorButton);
+
+        InlineKeyboardButton middleButton = new InlineKeyboardButton();
+        middleButton.setText("MIDDLE");
+        middleButton.setCallbackData("showMiddleVacancies");
+        proficiencyLevelButtons.add(middleButton);
+
+        InlineKeyboardButton seniorButton = new InlineKeyboardButton();
+        seniorButton.setText("SENIOR");
+        seniorButton.setCallbackData("showSeniorVacancies");
+        proficiencyLevelButtons.add(seniorButton);
+
+        InlineKeyboardMarkup proficiencyLevelKeyboard = new InlineKeyboardMarkup();
+        proficiencyLevelKeyboard.setKeyboard(List.of(proficiencyLevelButtons));
+
+        return proficiencyLevelKeyboard;
     }
 
     // Bot name that was registered in Telegram @BotFather
